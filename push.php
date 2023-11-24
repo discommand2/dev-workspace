3cf41d4322d628dfd0b05a5923cb1686b3d5aed9
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-11-24 10:11:39 +0000

    AutoSync

--- push.php
+++ push.php
@@ -3,25 +3,10 @@
 
 $dir = __DIR__;
 chdir($dir);
-
-function loop()
-{
-    // Add all changes
-    passthru("git submodule foreach --recursive git add -A");
-
-    // Commit all changes
-    passthru("git submodule foreach --recursive git commit -m 'AutoSync'");
-
-    // Push all changes
-    passthru("git submodule foreach --recursive git push origin master");
-
-    // Pull all changes
-    passthru("git submodule foreach --recursive git pull origin master");
-}
-
-loop();
-loop();
-loop();
+passthru("git submodule foreach --recursive git add -A");
+passthru("git submodule foreach --recursive git commit -m 'AutoSync'");
+passthru("git submodule foreach --recursive git push");
+passthru("git submodule foreach --recursive git pull");
 passthru("git add -A");
 passthru("git commit -m 'AutoSync'");
-passthru("git push origin master");
+passthru("git push");
