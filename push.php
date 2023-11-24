39fcef8f68158815fd3030000a0769110ce8a8ff
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-11-24 12:50:44 +0000

    AutoSync

--- push.php
+++ push.php
@@ -4,9 +4,9 @@
 $dir = __DIR__;
 chdir($dir);
 passthru("git submodule foreach --recursive git add -A");
-passthru("git submodule foreach --recursive git commit -m 'AutoSync'");
+passthru("git submodule foreach --recursive 'git diff --cached --exit-code || git commit -m AutoSync'");
 passthru("git submodule foreach --recursive git push");
 passthru("git submodule foreach --recursive git pull");
 passthru("git add -A");
-passthru("git commit -m 'AutoSync'");
+passthru("git diff --cached --exit-code || git commit -m AutoSync");
 passthru("git push");
