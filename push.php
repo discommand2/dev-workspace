3e490af2fd4e3b5f39be4b4e3511e6f815d9edf1
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-11-24 09:50:10 +0000

    AutoSync

--- push.php
+++ push.php
@@ -1,51 +1,27 @@
+#!/usr/bin/env php
 <?php
 
-// Function to recursively stage all unstaged files in a given directory
-function stageUnstagedFiles($directory)
+$dir = __DIR__;
+chdir($dir);
+
+function loop()
 {
-    $output = shell_exec("git -C $directory status --porcelain");
-    $files = explode("\n", trim($output));
+    // Add all changes
+    passthru("git submodule foreach --recursive git add -A");
 
-    foreach ($files as $file) {
-        if (strpos($file, '??') === 0) {
-            $filePath = trim(substr($file, 2));
-            shell_exec("git -C $directory add $filePath");
-        }
-    }
+    // Commit all changes
+    passthru("git submodule foreach --recursive git commit -m 'AutoSync'");
+
+    // Push all changes
+    passthru("git submodule foreach --recursive git push origin master");
+
+    // Pull all changes
+    passthru("git submodule foreach --recursive git pull origin master");
 }
 
-// Function to commit and push changes in a given directory
-function commitAndPush($directory, $message)
-{
-    shell_exec("git -C $directory commit -m \"$message\"");
-    shell_exec("git -C $directory push");
-}
-
-// Function to recursively sync all submodules in a given directory
-function syncSubmodules($directory)
-{
-    $output = shell_exec("git -C $directory submodule foreach --recursive git submodule sync");
-    echo $output;
-}
-
-// Main script
-$rootDirectory = __DIR__;
-
-// Stage and commit changes in submodules
-$output = shell_exec("git -C $rootDirectory submodule foreach --recursive git status");
-$submodules = explode("\n", trim($output));
-
-foreach ($submodules as $submodule) {
-    $submodulePath = trim(substr($submodule, 15));
-    stageUnstagedFiles($submodulePath);
-    commitAndPush($submodulePath, "AutoHandler");
-}
-
-// Sync submodules
-syncSubmodules($rootDirectory);
-
-// Stage and commit changes in the root workspace
-stageUnstagedFiles($rootDirectory);
-commitAndPush($rootDirectory, "AutoHandler");
-
-echo "Script executed successfully.";
+loop();
+loop();
+loop();
+passthru("git add -A");
+passthru("git commit -m 'AutoSync'");
+passthru("git push origin master");
